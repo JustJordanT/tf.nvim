7bfeaed287c9578331bba0985f6ad7ad7d008353
Jordan <Jordantay9014@outlook.com>
2023-09-21 23:08:28 -0500

    init

---
+++ lua/plugin.lua
@@ -0,0 +1,30 @@
+local plugin = {}
+
+plugin.commands = {
+  TerraformFmt = function()
+    -- Format the current Terraform file.
+    vim.cmd('!terraform fmt')
+  end,
+  TerraformFmtOnSave = function()
+    -- Format the current Terraform file on save.
+    vim.api.
+    vim.api.autocmd.Add('BufWritePre', {
+      group = 'terraform_fmt',
+      pattern = '*.tf',
+      callback = function()
+        vim.cmd('!terraform fmt')
+      end,
+    })
+  end,
+}
+
+plugin.keymaps = {
+  ['n <leader>tf'] = ':TerraformFmt<CR>',
+}
+
+plugin.setup = function()
+  -- Enable Terraform fmt on save.
+  vim.cmd('call plugin#terraform-fmt#TerraformFmtOnSave()')
+end
+
+return plugin
